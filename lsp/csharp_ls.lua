665c1788fb0cc48f4dead7a1aa67217957ed67a6
vctrthe <victorthe123@gmail.com>
2026-09-01 17:39:35 +0700

    init: neovim starter configs

---
+++ lsp/csharp_ls.lua
@@ -0,0 +1,43 @@
+---@brief
+---
+--- https://github.com/razzmatazz/csharp-language-server
+---
+--- Language Server for C#.
+---
+--- csharp-ls requires the [dotnet-sdk](https://dotnet.microsoft.com/download) to be installed.
+---
+--- The preferred way to install csharp-ls is with `dotnet tool install --global csharp-ls`.
+
+---@type vim.lsp.Config
+return {
+  cmd = function(dispatchers, config)
+    return vim.lsp.rpc.start({ 'csharp-ls' }, dispatchers, {
+      -- csharp-ls attempt to locate sln, slnx or csproj files from cwd, so set cwd to root directory.
+      -- If cmd_cwd is provided, use it instead.
+      cwd = config.cmd_cwd or config.root_dir,
+      env = config.cmd_env,
+      detached = config.detached,
+    })
+  end,
+  root_dir = function(bufnr, on_dir)
+    local root = vim.fs.root(bufnr, {
+      "*.sln",
+			"*.slnx",
+			"*.csproj",
+		})
+
+		if root then
+      on_dir(root)
+    end
+  end,
+  filetypes = { 'cs' },
+  init_options = {
+    AutomaticWorkspaceInit = true,
+  },
+  get_language_id = function(_, ft)
+    if ft == 'cs' then
+      return 'csharp'
+    end
+    return ft
+  end,
+}
---
+++ lua/options.lua
@@ -0,0 +1,11 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.signcolumn = "yes"
+vim.o.undofile = true
+vim.o.autoread = true
+vim.o.laststatus = 3
+vim.o.cmdheight = 0
